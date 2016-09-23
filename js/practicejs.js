6b01a7e1fbfc3bbbcb8c1de6bfc9655e4336ad5b
Leonardo Alberto Herrera Becerra <leonardo.herrera@globant.com>
2016-09-23 14:56:44 -0500

    Creating a Promise with a config object as parameter

--- js/practicejs.js
+++ js/practicejs.js
@@ -1,3 +1,4 @@
+//XMLHttpRequest to receive the Joke (used previously)
 function receiveMessage(url){
   var xmlRequest = new XMLHttpRequest();
   var spanResponse = document.getElementById('spanResponse');
@@ -15,4 +16,37 @@ function receiveMessage(url){
 
 function writeMessage(){
   var xmlResponse = receiveMessage('http://api.icndb.com/jokes/random');
+}
+
+//Using a Promise with an object as Parameter
+function sendRequest(conf) {
+    return new Promise(function(resolve, reject) {
+      var xmlRequest = new XMLHttpRequest();
+      xmlRequest.open('GET', conf.url);
+
+      xmlRequest.onload = function() {
+        if(xmlRequest.status == 200) {
+          resolve(xmlRequest.response);
+        }
+        else {
+          reject(Error(xmlRequest.statusText));
+        }
+      };
+
+      xmlRequest.onerror = function() {
+        reject(Error("Network Error"));
+      };
+
+      xmlRequest.send();
+    });
+}
+
+function receiveJoke(){
+  sendRequest({url:'http://api.icndb.com/jokes/random'}).then(function(response) {
+    jSONResponse =JSON.parse(response);
+    document.getElementById('spanResponse').innerHTML = jSONResponse.value.joke;
+  },function(error) {
+    document.getElementById('spanResponse').style = "color: red;";
+    document.getElementById('spanResponse').innerHTML = error;
+  });
 }
