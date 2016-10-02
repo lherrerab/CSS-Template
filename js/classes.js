e154dd2e22bfabc4cd6d1f7e5941b5ca90d15f97
Leonardo Herrera <herreraleonardo@hotmail.com>
2016-10-02 10:17:17 -0500

    Adding files 2.Classes

---
+++ js/classes.js
@@ -0,0 +1,20 @@
+import { EventEmitter } from 'events');
+
+// Factory shared
+var makePerson = function() {
+  let person = {};
+  EventEmitter.call(person);
+  person.wallet = 0;
+  Object.assign(person, personMethods)
+  return person;
+}
+let personMethods = {};
+Object.assign(personMethods, EventEmitter.prototype)
+
+personMethods.talk = function() {
+
+};
+
+let person = makePerson();
+person.talk();
+ou
